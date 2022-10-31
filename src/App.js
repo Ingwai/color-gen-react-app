d9f20a1776b911891fbbad795b82b9d272b7e7a8
Ingwai <ingwai@wp.pl>
2022-10-31 16:52:21 +0100

    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Values from 'values.js';
+import SingleColor from './SingleColor';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputColor, setInputColor] = useState('');
+	const [error, setError] = useState(false);
+	const [list, setList] = useState([]);
+
+	const inputColorHandler = e => {
+		setInputColor(e.target.value);
+	};
+
+	const submitHandler = e => {
+		setError(false);
+		e.preventDefault();
+		try {
+			let colors = new Values(inputColor).all(5);
+			setList(colors);
+		} catch (error) {
+			setError(true);
+			console.log(error.message);
+		}
+		setInputColor('');
+	};
+
+	return (
+		<>
+			<section className='container'>
+				<h3>Color Generator</h3>
+				<form onSubmit={submitHandler}>
+					<input
+						value={inputColor}
+						type='text'
+						placeholder='type color'
+						onChange={inputColorHandler}
+						className={`${error ? 'error' : 'null'}`}
+					/>
+					<button className='btn' type='submit'>
+						Get Colors
+					</button>
+				</form>
+			</section>
+			<section className='colors'>
+				{list.map((color, index) => (
+					<SingleColor key={index} {...color} index={index} hexColor={color.hex} />
+				))}
+			</section>
+		</>
+	);
 }
 
 export default App;
